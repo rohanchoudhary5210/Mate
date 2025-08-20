9f8e4fbd0d446268a748248bf1faa6f282d209c7
Rohan5210 <rohan5210work@gmail.com>
2025-08-20 14:30:21 +0530

    changes

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 
 public class Loader : MonoBehaviour
@@ -11,6 +14,7 @@ public class Loader : MonoBehaviour
     public Sprite[] backgrounds;
     public Sprite[] icons;
     public GameObject cardPrefab;
+    [SerializeField]
     public PlayerList playerList;
     public Transform cardContainer;
     int i = 0;
@@ -29,8 +33,34 @@ public class Loader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TextAsset data = Resources.Load<TextAsset>("players");
-        playerList = JsonUtility.FromJson<PlayerList>(data.text);
+
+        Initialize();
+       
+    }
+    public void LoadPlayerData(string jsonString, int bgindex)
+    {
+        Debug.Log("Loading player data: " + jsonString);
+        TextAsset data = Resources.Load<TextAsset>("NewJson");
+        JObject originalData = JObject.Parse(data.text);
+        Debug.Log("Original JSON: " + originalData.ToString());
+        JObject changeddata = JObject.Parse(jsonString);
+        Debug.Log("Changed JSON: " + changeddata.ToString());
+
+        originalData[bgindex] = changeddata;
+        string updatedJsonString = JsonConvert.SerializeObject(originalData, Formatting.Indented);
+        Debug.Log("Updated JSON: " + updatedJsonString);
+
+        File.WriteAllText(Application.persistentDataPath + "/NewJson.json", updatedJsonString);
+        Debug.Log("Data saved to: " + Application.persistentDataPath + "/NewJson.json");
+
+    }
+
+    // Update is called once per frame
+    public void Initialize()
+    {
+        TextAsset data = Resources.Load<TextAsset>("NewJson");
+        playerList = JsonConvert.DeserializeObject<PlayerList>(data.text);
+        Debug.Log("playerlist loaded" + playerList);
         foreach (player p in playerList.players)
         {
             GameObject cardObject = Instantiate(cardPrefab, cardContainer);
@@ -38,20 +68,15 @@ public class Loader : MonoBehaviour
             i++;
         }
     }
-
-    // Update is called once per frame
-    public void Initialize()
-    {
-        
-    }
 }
-[System.Serializable]
+[SerializeField]
 public class player
 {
     public string name;
-    public int level;
     public string id;
+    public int level;
     public int igindex;
+    public Dictionary<string, int> details;
 }
 [System.Serializable]
 public class PlayerList
--- Assets/Scripts/card.cs
+++ Assets/Scripts/card.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+using System.IO;
+using System.Data.Common;
+using Unity.VisualScripting;
+using UnityEditor;
 
 public class card : MonoBehaviour
 {
@@ -9,6 +14,9 @@ public class card : MonoBehaviour
     [SerializeField] private TextMeshProUGUI idText;
     [SerializeField] private Image iconImage;
     [SerializeField] private Image backgroundImage;
+    [SerializeField] private TextMeshProUGUI details;
+    [SerializeField] private Button button;
+
 
     // bgindex is passed as an argument, so it's a local parameter
     public void InitializeCardData(player data, int bgindex)
@@ -18,5 +26,24 @@ public class card : MonoBehaviour
         levelText.text = data.level.ToString();
         iconImage.sprite = Loader.instance.icons[data.igindex];
         backgroundImage.sprite = Loader.instance.backgrounds[bgindex];
+        if (button.onClick != null)
+        {
+            button.onClick.AddListener(() => { work(data, bgindex); });
+
+            
+        }
+    }
+
+    public void work(player data, int bgindex)
+    {
+        data.details["X value"] = data.level;
+        data.details["Y value"] = bgindex;
+        details.text = $"X: {data.details["X value"]}, Y: {data.details["Y value"]}";
+        string jsonString = JsonConvert.SerializeObject(data, Formatting.Indented);
+        Loader.instance.LoadPlayerData(jsonString,bgindex);
+        // Debug.Log("Serialized player data: " + jsonString);
+        // Debug.Log(jsonString);
+        // string filePath = Application.persistentDataPath + "D:/Co/C/Assets/Resources/NewJson.json";
+        // File.WriteAllText(filePath, jsonString);
     }
 }
